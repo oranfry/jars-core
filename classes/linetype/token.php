012e2a6117e03faf513c8bb288627522e6b8ddfc
Oran Fry <oran@emerg.co>
2023-05-04 21:17:49 +1200

    Token Linetype
    Rewrite in raw fuse / unfuse style.

--- classes/linetype/token.php
+++ classes/linetype/token.php
@@ -8,14 +8,18 @@ class token extends \jars\Linetype
     {
         $this->table = 'token';
 
-        $this->simple_hex('token');
-        $this->simple_int('ttl');
-        $this->simple_int('hits', 0);
+        $this->fields = [
+            'token' => fn ($records): ?string => ($base64 = $records['/']->token) ? bin2hex(base64_decode($base64)) : null,
+            'ttl' => fn ($records): ?int => $records['/']->ttl,
+            'used' => fn ($records): int => $records['/']->used,
+            'expired' => fn ($records): bool => strtotime($records['/']->used) + $records['/']->ttl > time(),
+        ];
 
-        $this->fields['used'] = fn ($records): int => $records['/']->used;
-        $this->unfuse_fields['used'] = fn ($line, $oldline): int => time();
-
-        $this->fields['expired'] = fn ($records): bool => strtotime($records['/']->used) + $records['/']->ttl > time();
+        $this->unfuse_fields = [
+            'token' => fn ($line): ?string => (false !== $bin = @hex2bin(@$line->token)) ? base64_encode($bin) : null,
+            'ttl' => fn ($line, $oldline): ?int => is_numeric(@$line->ttl) ? (int) $line->ttl : 0,
+            'used' => fn ($line, $oldline): int => time(),
+        ];
     }
 
     function complete($line): void
