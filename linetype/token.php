bba3834de6d757c8b2c7214e7a9df2827299d0b1
Oran Fry <oran@emerg.co>
2024-07-26 20:52:25 +1200

    Tokens
    Add delete protection mechanism.

--- linetype/token.php
+++ linetype/token.php
@@ -13,17 +13,31 @@ class token extends \jars\Linetype
             'ttl' => fn ($records): ?int => $records['/']->ttl,
             'used' => fn ($records): int => $records['/']->used,
             'expired' => fn ($records): bool => strtotime($records['/']->used) + $records['/']->ttl > time(),
+            'delete_protected' => fn ($records): bool => $records['/']->delete_protected ?? false,
         ];
 
         $this->unfuse_fields = [
-            'token' => fn ($line): ?string => (false !== $bin = @hex2bin(@$line->token)) ? base64_encode($bin) : null,
+            'token' => fn ($line): ?string => (false !== $bin = @hex2bin($line->token)) ? base64_encode($bin) : null,
             'ttl' => fn ($line, $oldline): ?int => is_numeric(@$line->ttl) ? (int) $line->ttl : 0,
             'used' => fn ($line, $oldline): int => time(),
+            'delete_protected' => fn ($line, $oldline): bool => (bool) $line->delete_protected,
         ];
     }
 
     function complete($line): void
     {
-        $line->ttl = $line->ttl ?? 86400;
+        $line->ttl ??= 86400;
+        $line->delete_protected ??= false;
+    }
+
+    public function validate($line): array
+    {
+        $is = $line->_is ?? true;
+
+        if (!$is && $line->delete_protected) {
+            return ['You cannot delete a protected token'];
+        }
+
+        return parent::validate($line);
     }
 }
